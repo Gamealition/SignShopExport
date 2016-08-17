4c0a6d8a3d92bbf528d298bdd9ddff832ae91bae
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2016-08-17 08:37:20 +0100

    Change outputFile to outputPath

--- src/main/java/roycurtis/signshopexport/Config.java
+++ src/main/java/roycurtis/signshopexport/Config.java
@@ -2,8 +2,6 @@ package roycurtis.signshopexport;
 
 import org.bukkit.configuration.Configuration;
 
-import java.util.logging.Logger;
-
 /**
  * Container class for plugin's configuration values
  */
@@ -12,17 +10,15 @@ class Config
     Configuration config;
 
     /** Path of data file to export, relative to plugin directory */
-    String outputFile = "data.json";
+    String outputPath = "data.json";
 
     Config(SignShopExport plugin)
     {
-        Logger LOGGER = SignShopExport.LOGGER;
-
         plugin.saveDefaultConfig();
         plugin.reloadConfig();
 
         config = plugin.getConfig();
 
-        outputFile = config.getString("outputFile", outputFile);
+        outputPath = config.getString("outputPath", outputPath);
     }
 }
